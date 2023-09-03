0124bcae0a85aa3f0a0f6d1f97ca62c12bc287ac
sj122508 <jamesdviojan@gmail.com>
2023-09-04 07:23:29 +0800

    Adjustment on table codes and add edit button

--- src/components/presentationals/table/tableBody/tableBody.js
+++ src/components/presentationals/table/tableBody/tableBody.js
@@ -4,9 +4,12 @@ import MUITableBody from '@mui/material/TableBody';
 import TableRow from '@mui/material/TableRow';
 import TableCell from '@mui/material/TableCell';
 import Checkbox from '@mui/material/Checkbox';
+import IconButton from '@mui/material/IconButton';
+import EditIcon from '@mui/icons-material/Edit';
+
 
 function TableBody(props) {
-    const { headerCells, onRowClick, rows, selected, tableOnly } = props;
+    const { headerCells, onRowClick, onEditClick, rows, selected, tableOnly } = props;
 
     const isSelected = (id) => {
         return selected.find(row => row.id === id) !== undefined;
@@ -15,25 +18,54 @@ function TableBody(props) {
     const populateCells = (row) => {
         return headerCells.map((cell, index) => {
 
-            if (cell.id === "id") return null;
+            if (cell.id === "id") return null; // do not display id column
 
             var rowCellName = Object.keys(row)[index]
 
             return (
                 <TableCell 
                     align= {cell.numeric ? 'right' : 'left'}
-                    key={row[rowCellName]}>
+                    key={row[rowCellName]}
+                    sx={{color: "#616161" }}
+                    >
                         {row[rowCellName]}
+                    
                 </TableCell>
             )
         })
+    }
+
+    const populateCellsCheckbox = (row) => {
+        const isItemSelected = isSelected(row.id);
+        return (
+             !tableOnly && 
+                <TableCell padding="checkbox">
+                    <Checkbox color="primary"
+                        checked={isItemSelected}
+                        inputProps={{
+                        'aria-labelledby': `table-checkbox-${row.name}`,
+                        }}
+                    />
+                </TableCell>
+        )
+    
+    }
+
+    const populateCellsEdit = (row) => {
+        return (
+            !tableOnly && 
+                <TableCell align="center">
+                    <IconButton aria-label={`table-edit-${row.name}`} onClick={() => onEditClick(row)}>
+                        <EditIcon fontSize='small'/>
+                    </IconButton>
+                </TableCell> 
+        )
     }
 
   return (
     <MUITableBody>
         {rows.map((row, index) => {
             const isItemSelected = isSelected(row.id);
-            const labelId = `enhanced-table-checkbox-${index}`;
 
             return (
                 <TableRow 
@@ -46,18 +78,11 @@ function TableBody(props) {
                     selected={isItemSelected}
                     sx={{ cursor: 'pointer' }}
                 >
-                    {
-                        !tableOnly && <TableCell padding="checkbox">
-                            <Checkbox color="primary"
-                                checked={isItemSelected}
-                                inputProps={{
-                                'aria-labelledby': labelId,
-                                }}
-                            />
-                        </TableCell>
-                    }
-                    
+                   
+                    {populateCellsCheckbox(row)}
                     {populateCells(row)}
+                    {populateCellsEdit(row)}
+                    
                 </TableRow>
             )
         })}
